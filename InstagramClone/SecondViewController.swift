a9973c83fbeea3e44e19b6d705f43aedae4520c4
Ravisara S. Kariyawasam <ravisara@gmail.com>
2020-05-18 16:38:32 +0100

    Initial Commit

---
+++ InstagramClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InstagramClone
+//
+//  Created by StarChanger on 18/05/2020.
+//  Copyright © 2020 StarChanger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
